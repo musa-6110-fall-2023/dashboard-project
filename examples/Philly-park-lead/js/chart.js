21da86957c5c3b9e896818f87a16af60f8461ea8
junyi2022 <junyi2022@berkeley.edu>
2023-10-17 22:55:06 -0400

    style layout and raw data

---
+++ examples/Philly-park-lead/js/chart.js
@@ -0,0 +1,22 @@
+const leadLevelMarker = document.querySelector('#lead-level-chart .level');
+const leadLevelLabel = document.querySelector('#lead-level-chart .level-label');
+
+function setLeadLevel(n) {
+  const maxLeadLevel = 18064;
+  const scaledValue = Math.log(n) / Math.log(maxLeadLevel) * 100;
+
+  // Set the location of the marker
+  leadLevelMarker.style.width = `${scaledValue}%`;
+
+  // Set the text of the label
+  leadLevelLabel.innerHTML = `${n} ppm`;
+
+  // Set the location of the label
+  const labelW = leadLevelLabel.offsetWidth;
+  leadLevelLabel.style.left = `min(calc(100% - ${labelW + 1}px), ${scaledValue}%)`;
+}
+
+export {
+  setLeadLevel,
+};
+
--- examples/Philly-park-lead/js/main.js
+++ examples/Philly-park-lead/js/main.js
@@ -1,3 +1,9 @@
 import { initializeMap } from './map.js';
+import { setLeadLevel } from './chart.js';
 
+const phillyPark = await fetch('data/philly-park.json');
+const parks = await phillyPark.json();
+
+window.parks = parks;
 window.schoolMap = initializeMap();
+window.setLeadLevel = setLeadLevel;
