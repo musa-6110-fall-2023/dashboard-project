325ffb96d37a4fb81c334b979ed0905da6ddc3e5
Van Archer <wx7@upenn.edu>
2023-12-22 16:15:25 -0500

    12/22

--- js/map.js
+++ js/map.js
@@ -1,9 +1,9 @@
 // =================================================================================================================> 1. initializeMap
-function initializeMap(stationInfo, events) {
-  // ===============================================================================> 1.1 initialize the basic map with center configured
+function initializeMap(landmarkData, events) {
+  // ==> 1.1 initialize the basic map with center configured
   const map = L.map('map').setView([39.95, -75.16], 12);
 
-  // ===============================================================================> 1.2 configure layer 1: tile layer from mapbox
+  // ==> 1.2 configure layer 1: tile layer from mapbox
   const mapboxToken = "pk.eyJ1IjoidmFuYXJjaCIsImEiOiJjbHBwbDhvejMwMXR3MmttazltNWVqYzNoIn0.c29HryOvTsGTu268w2uclw"; 
   const mapboxAccount = "vanarch"; 
   const mapboxStyleID = "clpr4jcme00on01p78i7zh45a"; 
@@ -16,25 +16,25 @@ function initializeMap(stationInfo, events) {
     baseTileLayer.addTo(map);
 
 
-  // ===============================================================================> 1.3 configure layer 2: marker layer configuration
-  const stationsLayer = L.layerGroup();
-  stationsLayer.addTo(map);
+  // ==> 1.3 configure layer 2: marker layer configuration
+  const landmarkLayer = L.layerGroup();
+  landmarkLayer.addTo(map);
 
 
-  // ===============================================================================> 1.4 add makers into layer 2 by calling function `updateMapStations()`
+  // ==> 1.4 add makers into layer 2 by calling function `updateMapLandmarks()`
 
-  updateMapStations(stationInfo.data.stations, stationsLayer);
+  updateMapLandmarks(landmarkData.data.stations, landmarkLayer);
 
-  // ===============================================================================> 1.5 configured three events to face specific events
-  // ==================================================================> 1.5.1 update map with filtered stations
+  // ==> 1.5 configured three events to face specific events
+  // => 1.5.1 update map with filtered landmarks
   events.addEventListener('filter-stations', (evt) => {
     const filteredStations = evt.detail.filteredStations;
-    updateMapStations(filteredStations, stationsLayer);
+    updateMapLandmarks(filteredStations, landmarkLayer);
   });
 
-  // ==================================================================> 1.5.2 zoom the map to display three nearest stations to user's current stations
+  // => 1.5.2 zoom the map to display three nearest landmarks to user's current landmarks
   events.addEventListener('geolocated', (evt) => {
-    // This listener will zoom to contain the three nearest stations to the
+    // This listener will zoom to contain the three nearest landmarks to the
     // user's current position.
 
     const pos = evt.detail;
@@ -44,7 +44,7 @@ function initializeMap(stationInfo, events) {
     // Create a turf point from the user's position. turf is a really cool library
     const userPoint = turf.point([lon, lat]);
 
-    // Create a comparison function for sorting stations by their distance from
+    // Create a comparison function for sorting landmarks by their distance from
     // the users position.
     function compareDists(stationA, stationB) {
       const stationAPoint = turf.point([stationA.lon, stationA.lat]);
@@ -55,16 +55,16 @@ function initializeMap(stationInfo, events) {
       return distA - distB;
     }
 
-    // Make a copy of the stations array with the slice function, and sort it.
+    // Make a copy of the landmarks array with the slice function, and sort it.
     // We make a copy first here because the JS array sort function sorts the
     // array in place, rather than returning a new array, and we want to keep
-    // the original stations array unchanged.
-    const sortedStations = stationInfo.data.stations.slice();
+    // the original landmarks array unchanged.
+    const sortedStations = landmarkData.data.stations.slice();
     sortedStations.sort(compareDists);
 
-    // Get the three closest stations from the sorted array, and use the map
+    // Get the three closest landmarks from the sorted array, and use the map
     // function to create a multipoint containing the coordinates of those
-    // stations.
+    // landmarks.
     const closestStations = sortedStations.slice(0, 3);
     const closestPoints = turf.multiPoint(closestStations.map((station) => [station.lon, station.lat]));
 
@@ -75,14 +75,14 @@ function initializeMap(stationInfo, events) {
     const bbox = turf.bbox(closestPoints);
     const leafletBbox = L.latLngBounds([bbox[1], bbox[0]], [bbox[3], bbox[2]]);
 
-    // Finally, fit the map view to the stations.
+    // Finally, fit the map view to the landmarks.
     map.fitBounds(leafletBbox);
   });
 
-  // ==================================================================> 1.5.3 open a pop up for a specific marker
+  // =>  1.5.3 open a pop up for a specific marker
   events.addEventListener('focus-station', (evt) => {
     const stationId = evt.detail.stationId;
-    stationsLayer.eachLayer((layer) => {
+    landmarkLayer.eachLayer((layer) => {
       if (layer.stationId === stationId) {
         layer.bindPopup('hello');
         layer.openPopup();
@@ -93,15 +93,15 @@ function initializeMap(stationInfo, events) {
   return map;
 }
 
-// =================================================================================================================> 2. updateMapStations
-function updateMapStations(stations, stationsLayer) {
-  // ===============================================================================> 2.1 clear all existing markers from stationsLayer 
-  stationsLayer.clearLayers();
+// =================================================================================================================> 2. updateMapLandmarks
+function updateMapLandmarks(stations, landmarkLayer) {
+  // ==> 2.1 clear all existing markers from landmarkLayer 
+  landmarkLayer.clearLayers();
 
-  // ===============================================================================> 2.2 debug helper
+  // ==> 2.2 debug helper
   // console.log(`Adding ${stations.length} stations to the map.`);
 
-  // ===============================================================================> 2.3 marker configuration including shape, size and anchor
+  // ==> 2.3 marker configuration including shape, size and anchor
   const stationIcon = L.icon({
     iconUrl: '_images/landmarker.png',
     iconSize: [32, 40], // size of the icon
@@ -109,7 +109,7 @@ function updateMapStations(stations, stationsLayer) {
     popupAnchor: [0, -35], // point from which the popup should open relative to the iconAnchor
   });
 
-  // ===============================================================================> 2.4 tailor each marker via `for` loop
+  // ==> 2.4 tailor each marker via `for` loop
 
   for (const station of stations) {
     const marker = L.marker([station.lat, station.lon], {    // use data's lat and lon
@@ -122,7 +122,7 @@ function updateMapStations(stations, stationsLayer) {
       <p class="station-address">${station.address}</p>
     `);                                                      // set up popup (connected with html)
     marker.stationId = station.station_id;                   // set up id
-    marker.addTo(stationsLayer);                             // add all these data into the stationsLayer
+    marker.addTo(landmarkLayer);                             // add all these data into the landmarkLayer
   }
 }
 
